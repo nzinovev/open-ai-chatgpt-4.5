d1eb9f0868bd5f770083feac7e79aba42faca1aa
n.zinovev <nervous9339@gmail.com>
2025-01-23 22:19:24 +0300

    init app

---
+++ src/main/java/com/example/chat/gpt/poe/ChatGptPoeApplication.java
@@ -0,0 +1,13 @@
+package com.example.chat.gpt.poe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatGptPoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatGptPoeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chat/gpt/poe/ChatGptPoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chat.gpt.poe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatGptPoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
